732aa73bd8f2c57817f119a45998bd163f77ef35
Fabian Bucher <git@fabianbucher.de>
2021-11-17 15:41:45 +0100

    absence: create absence entity

---
+++ server/src/entity/Absence.ts
@@ -0,0 +1,48 @@
+import { ID, ObjectType, Field } from 'type-graphql'
+import { BaseEntity, Column, CreateDateColumn, Entity, OneToMany, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+
+@Entity()
+@ObjectType()
+export class Absence extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    @Field(() => ID)
+    id: string
+
+    @CreateDateColumn()
+    @Field()
+    createdAt: number
+
+    @UpdateDateColumn()
+    @Field()
+    updatedAt: number
+
+    /**
+     * Meant is the lesson index (starting at 1) throughout the school day
+     * Examples:
+     * '1. Schulstunde' -> 1
+     */
+    @Column()
+    @Field()
+    lessonIndex: number
+
+    /**
+     * Example: 2021-01-25 (ISO string)
+     */
+    @Column('date')
+    @Field()
+    date: string
+
+    @Column()
+    @Field()
+    excused: boolean
+
+    @Column()
+    @Field()
+    processed: boolean
+
+    @ManyToOne(() => Absence, absence => absence.grouping)
+    groupedBy: Absence;
+
+    @OneToMany(() => Absence, absence => absence.groupedBy)
+    grouping: Absence[];
+}
