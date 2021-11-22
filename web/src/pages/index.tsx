1484aeed9eca9763e607e4f3b24c315f1357317e
robKoh <robKoh@github.com>
2021-11-22 18:47:10 +0100

    Changed Box to LinkBox and LinkOverlay in centered GridLayout. Now you can click on image and another page will be opening.

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,8 @@
-import { Flex, Spacer, Image, Text, Heading, GridItem, Grid, Box, SimpleGrid, Divider, Container, Center } from "@chakra-ui/react"
+import {
+  Flex, Spacer, Image, Text, Heading, GridItem,
+  Grid, Box, SimpleGrid, Divider, Container, Center,
+  LinkBox, LinkOverlay
+} from "@chakra-ui/react"
 import { PageScaffold } from '../components/PageScaffold'
 import WithAuth, { WithAuthProps } from '../components/withAuth'
 
@@ -38,71 +42,79 @@ const Index: React.FC<WithAuthProps> = ({ self }) => {
         </Box>
         <Box marginTop="36" marginLeft="24" marginRight="24">
           <SimpleGrid minChildWidth="325px" spacing="40px">
-              <Box
-                bg="#56CCF2" color="white" width="325px" height="235px" fontSize="larger"
-                textAlign="center" borderStyle="solid" borderWidth="thin" borderColor="black"
-              >
-                <Center marginTop="4">
-                  <Image
-                    objectFit="cover"
-                    src="https://i.ibb.co/BjYJF0L/Tutor.png"
-                    height="44"
-                    alt="Tutor-Bild"
-                  />
-                </Center>
-                <b>
+            <LinkBox
+              bg="#56CCF2" color="white" width="325px" height="235px" fontSize="larger"
+              textAlign="center" borderStyle="solid" borderWidth="thin" borderColor="black"
+            >
+              <Center marginTop="4">
+                <Image
+                  objectFit="cover"
+                  src="https://i.ibb.co/BjYJF0L/Tutor.png"
+                  height="44"
+                  alt="Tutor-Bild"
+                />
+              </Center>
+              <b>
+                <LinkOverlay href="/tutorium">
                   Tutorium Management
-                </b>
-              </Box>
-              <Box
-                bg="#FC912A" color="white" width="325px" height="235px" fontSize="larger"
-                textAlign="center" borderStyle="solid" borderWidth="thin" borderColor="black"
-              >
-                <Center marginTop="4">
-                  <Image
-                    objectFit="cover"
-                    src="https://i.ibb.co/rtsrGTx/Sch-ler.png"
-                    height="44"
-                    alt="Schüler-Bild"
-                  />
-                </Center>
-                <b>
+                </LinkOverlay>
+              </b>
+            </LinkBox>
+            <LinkBox
+              bg="#FC912A" color="white" width="325px" height="235px" fontSize="larger"
+              textAlign="center" borderStyle="solid" borderWidth="thin" borderColor="black"
+            >
+              <Center marginTop="4">
+                <Image
+                  objectFit="cover"
+                  src="https://i.ibb.co/rtsrGTx/Sch-ler.png"
+                  height="44"
+                  alt="Schüler-Bild"
+                />
+              </Center>
+              <b>
+                <LinkOverlay href="/">
                   Schüler Management
-                </b>
-              </Box>
-              <Box
-                bg="#94E43B" color="white" width="325px" height="235px" fontSize="larger"
-                textAlign="center" borderStyle="solid" borderWidth="thin" borderColor="black"
-              >
-                <Center marginTop="4">
-                  <Image
-                    objectFit="cover"
-                    src="https://i.ibb.co/GPvKhRF/Abwesenheit-buchen.png"
-                    alt="Abwesenheit-Bild"
-                    height="44"
-                  />
-                </Center>
-                <b>
+                </LinkOverlay>
+              </b>
+            </LinkBox>
+            <LinkBox
+              bg="#94E43B" color="white" width="325px" height="235px" fontSize="larger"
+              textAlign="center" borderStyle="solid" borderWidth="thin" borderColor="black"
+            >
+              <Center marginTop="4">
+                <Image
+                  objectFit="cover"
+                  src="https://i.ibb.co/GPvKhRF/Abwesenheit-buchen.png"
+                  alt="Abwesenheit-Bild"
+                  height="44"
+                />
+              </Center>
+              <b>
+                <LinkOverlay href="/">
                   Abwesenheit buchen
-                </b>
-              </Box>
-              <Box
-                bg="#9B51E0" color="white" width="325px" height="235px" fontSize="larger"
-                textAlign="center" borderStyle="solid" borderWidth="thin" borderColor="black"
-              >
-                <Center >
-                  <Image
-                    objectFit="cover"
-                    src="https://i.ibb.co/dDWNbHf/Support.png"
-                    height="48"
-                    alt="Support-Bild"
-                    marginBottom="-0.5"
-                  />
-                </Center>
-                <b>
+                </LinkOverlay>
+              </b>
+            </LinkBox>
+            <LinkBox
+              bg="#9B51E0" color="white" width="325px" height="235px" fontSize="larger"
+              textAlign="center" borderStyle="solid" borderWidth="thin" borderColor="black"
+            >
+              <Center >
+                <Image
+                  objectFit="cover"
+                  src="https://i.ibb.co/dDWNbHf/Support.png"
+                  height="48"
+                  alt="Support-Bild"
+                  marginBottom="-0.5"
+                />
+              </Center>
+              <b>
+                <LinkOverlay href="/">
                   Support
-                </b>
-              </Box>
+                </LinkOverlay>
+              </b>
+            </LinkBox>
           </SimpleGrid>
         </Box>
       </Box>
