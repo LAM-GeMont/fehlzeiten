bbe7c2ab95a7fa9a7d69c5a517c6f04576442fe5
jonaslanglotz <jonas.l1512@gmail.com>
2021-11-26 16:11:50 +0100

    Removed unused enum values

--- server/src/resolvers/absence/forStudent.ts
+++ server/src/resolvers/absence/forStudent.ts
@@ -7,7 +7,6 @@ import { Context } from '../../types'
 export enum AbsencesForStudentErrorCode {
   UNKNOWN_ERROR,
   INVALID_STUDENT_ID,
-  UNAUTHORIZED
 }
 
 registerEnumType(AbsencesForStudentErrorCode, {
