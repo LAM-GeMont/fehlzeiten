21c6cd76f7697f8de06951ca46aecb24176a4dca
robKoh <robertkohn00@gmail.com>
2021-11-28 18:14:43 +0100

    Code cosmetics

--- web/src/components/LinkBoxHomePage.tsx
+++ web/src/components/LinkBoxHomePage.tsx
@@ -12,20 +12,19 @@ interface Props {
 export const LinkBoxHomePage: React.FC<Props> = (props) => {
   return (
     <LinkBox
-        bg='#001955' color='white' width='inherit' height='inherit' fontSize='inherit'
-        textAlign='center' borderRadius='25px' 
+    bg='#001955' color='white' width='inherit' height='inherit' fontSize='inherit'
+    textAlign='center' borderRadius='25px' 
     >
       <Center>
         <Box m={4}>
-          <Icon boxSize={{ base: 24, sm: 36, md: 36, lg: 40 }} as={props.icon}> 
-          </Icon>
-            <Box>
-              <b>
-                <LinkOverlay href={props.href}>
-                  {props.text}
-                </LinkOverlay>
-              </b>
-            </Box>
+          <Icon boxSize={{ base: 24, sm: 36, md: 36, lg: 40 }} as={props.icon}></Icon>
+          <Box>
+            <b>
+              <LinkOverlay href={props.href}>
+                {props.text}
+              </LinkOverlay>
+            </b>
+          </Box>
         </Box>
       </Center>
     </LinkBox>
