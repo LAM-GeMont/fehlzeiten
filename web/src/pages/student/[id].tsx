b697a828569df440c4a73a7b0c450bc86ba45b68
jonaslanglotz <jonas.l1512@gmail.com>
2021-11-30 13:11:08 +0100

    Changed frontend to use single object queries

--- web/src/pages/student/[id].tsx
+++ web/src/pages/student/[id].tsx
@@ -1,13 +1,14 @@
 import { AddIcon, DeleteIcon, RepeatIcon } from '@chakra-ui/icons'
-import { Box, Button, Flex, Heading, IconButton, SimpleGrid, Spinner, Text, useDisclosure, useToast } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, IconButton, SimpleGrid, Spinner, Text, useDisclosure, useToast, Center, AlertIcon } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import React, { useMemo } from 'react'
 import { PageScaffold } from '../../components/PageScaffold'
 import SortedTable from './SortedTableAbsences'
 import WithAuth, { WithAuthProps } from '../../components/withAuth'
-import { Role, useAbsencesForStudentQuery, useStudentsQuery } from '../../generated/graphql'
+import { Role, useAbsencesForStudentQuery } from '../../generated/graphql'
 import { toastApolloError } from '../../util'
 import { DeleteAbsenceAlertDialog } from '../../components/DeleteAbsenceAlertDialog'
+import ErrorPage from 'next/error'
 
 interface Props extends WithAuthProps { }
 
@@ -20,51 +21,15 @@ const Student: React.FC<Props> = ({ self }) => {
 
   const toast = useToast()
 
-  const studentsQuery = useStudentsQuery({
+  const studentQuery = useAbsencesForStudentQuery({
+    variables: {
+      studentId: id.toString()
+    },
     onError: errors => toastApolloError(toast, errors)
   })
 
-  const studentAbsences = useAbsencesForStudentQuery({
-    variables: {
-      studentId: id.toString()
-    }
-  })
-
-  const studentsData = useMemo(() => {
-    if (studentsQuery.data?.students != null) {
-      return studentsQuery.data.students
-    } else {
-      return []
-    }
-  }, [studentsQuery.data])
-
-  const absenceData = useMemo(() => {
-    if (studentAbsences.data?.absencesForStudent.absences != null) {
-      return studentAbsences.data.absencesForStudent.absences
-    } else {
-      return []
-    }
-  }, [studentAbsences.data])
-
-  let firstName = ''
-  let lastName = ''
-  let tutorium = ''
-  let tutorId = ''
-
-  function getStudent () {
-    studentsData.forEach(e => {
-      if (e.id === id) {
-        firstName = (e.firstName)
-        lastName = (e.lastName)
-        if (e.tutorium !== null) {
-          tutorium = (e.tutorium.name)
-          if (e.tutorium.tutor !== null) {
-            tutorId = (e.tutorium.tutor.id)
-          }
-        }
-      }
-    })
-  }
+  const absences = studentQuery.data?.student?.absences || []
+  const student = studentQuery.data?.student
 
   const columns = useMemo(() => [
     {
@@ -87,75 +52,62 @@ const Student: React.FC<Props> = ({ self }) => {
       Header: 'Aktionen',
       Cell: ({ row }) => (
         <Flex justifyContent="center">
-          <IconButton isDisabled={self.role !== Role.Coordinator && row.original.submittedBy !== self.id && tutorId !== self.id} variant="outline" aria-label="Löschen" icon={<DeleteIcon />} onClick={() => {
+          <IconButton isDisabled={self.role !== Role.Coordinator && row.original.submittedBy !== self.id && student.tutorium?.tutor?.id !== self.id} variant="outline" aria-label="Löschen" icon={<DeleteIcon />} onClick={() => {
             setRowId(row.original.id)
             absenceDeleteAlertDialog.onOpen()
           }} />
         </Flex>
       )
     }
-  ], [absenceDeleteAlertDialog, self.id, self.role, tutorId])
+  ], [absenceDeleteAlertDialog, self.id, self.role, student])
 
-  const dates = []
+  const dates = Array.from(new Set(absences.map(absence => absence.date))).sort().reverse()
 
-  function getAbsencesDates () {
-    absenceData.forEach(e => {
-      dates.push(e.date)
-    })
-
-    const unique = [...Array.from(new Set(dates))]
-    unique.sort()
-    unique.reverse()
-
-    return unique
+  if (studentQuery.loading) {
+    return <Center h="100vh"><Spinner /></Center>
   }
 
-  function getAbsenceForDate (date: string) {
-    return absenceData.filter(absenceData => absenceData.date === date)
+  if (studentQuery.error != null) {
+    return (
+      <PageScaffold role={self.role}>
+        <Center h="100vh" color="red">
+          <AlertIcon />
+          <Heading>Fehler beim Laden der Daten</Heading>
+        </Center>
+      </PageScaffold>
+    )
   }
 
-  function checkIfAbsencesDataIsEmpty () {
-    if (absenceData.length > 0) {
-      return (
-        <Flex w="full" padding={5}>
-          <Text fontSize="24" fontWeight="bold">Fehlzeiten</Text>
-          <Button marginLeft="auto" leftIcon={<AddIcon />} /* onClick={TODO} */>Entschuldigung hinzufügen</Button>
-          <IconButton ml={4} variant="outline" aria-label="Daten neu laden" icon={<RepeatIcon />} onClick={() => { studentAbsences.refetch() }}></IconButton>
-        </Flex>
-      )
-    } else {
-      return (
-        <Flex w="full" padding={5}>
-          <Text fontSize="24" fontWeight="bold">Es wurden noch keine Fehlzeiten erfasst...</Text>
-        </Flex>
-      )
-    }
+  if (student == null) {
+    <ErrorPage statusCode={404} />
   }
 
   return (
     <PageScaffold role={self.role}>
-      {console.log(getAbsencesDates())}
-      {getStudent()}
-
       <SimpleGrid>
-        <Text fontSize="30" fontWeight="bold">{firstName + ' ' + lastName}</Text>
-        <Text fontSize="26">{tutorium}</Text>
+        <Text fontSize="30" fontWeight="bold">{student.firstName + ' ' + student.lastName}</Text>
+        <Text fontSize="26">{student.tutorium?.name}</Text>
         <Flex direction="column" alignItems="center" minW="300px" minH="600px" margin={5}>
-          {checkIfAbsencesDataIsEmpty()}
-          {studentAbsences.loading && (<Spinner />)}
-          {studentAbsences.error != null && (<Heading>Error!</Heading>)}
-          {studentAbsences.data != null && (
-            getAbsencesDates().map(function (each) {
-              const year = each.substring(0, 4)
-              const month = each.substring(5, 7)
-              const day = each.substring(8, 10)
-              console.log(each)
-              return (
-                <Box key={each} w="full" border="1px" borderColor="gray.300" borderRadius="md" boxShadow="lg" p="6" rounded="md" bg="white" mb={4}>
-                  <Text fontSize="22" pl={2}>{day}.{month}.{year}</Text>
-                  <SortedTable columns={columns} data={getAbsenceForDate(each)} />
-                </Box>)
-            })
+          {dates.length < 1 &&
+            <Flex w="full" padding={5}>
+              <Text fontSize="24" fontWeight="bold">Es wurden noch keine Fehlzeiten erfasst...</Text>
+            </Flex>
+          }
+          {dates.length >= 1 && (
+            <>
+              <Flex w="full" padding={5}>
+                <Text fontSize="24" fontWeight="bold">Fehlzeiten</Text>
+                <Button marginLeft="auto" leftIcon={<AddIcon />} /* onClick={TODO} */>Entschuldigung hinzufügen</Button>
+                <IconButton ml={4} variant="outline" aria-label="Daten neu laden" icon={<RepeatIcon />} onClick={() => { studentQuery.refetch() }}></IconButton>
+              </Flex>
+              {dates.map(date => {
+                return (
+                  <Box key={date} w="full" border="1px" borderColor="gray.300" borderRadius="md" boxShadow="lg" p="6" rounded="md" bg="white" mb={4}>
+                    <Text fontSize="22" pl={2}>{new Date(date).toLocaleDateString()}</Text>
+                    <SortedTable columns={columns} data={absences.filter(absence => absence.date === date)} />
+                  </Box>)
+              })}
+            </>
           )}
         </Flex>
       </SimpleGrid>
