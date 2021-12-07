1e95d2aa966e0e083f85dfcc160748753c5feed0
jonaslanglotz <jonas.l1512@gmail.com>
2021-12-07 17:02:10 +0100

    Added fetch policy to default options

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -7,7 +7,12 @@ import { AppProps } from 'next/app'
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
-  credentials: 'include'
+  credentials: 'include',
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network'
+    }
+  }
 })
 
 function MyApp ({ Component, pageProps }: AppProps) {
